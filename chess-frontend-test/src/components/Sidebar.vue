0fd72091d984fdb23ca4c967ec1ea0963655610e
kynhix <kristopher.t.hicks@gmail.com>
2024-04-21 16:21:59 -0400

    Updated animations for sidebar and added title

--- chess-frontend-test/src/components/Sidebar.vue
+++ chess-frontend-test/src/components/Sidebar.vue
@@ -6,27 +6,41 @@ const props = defineProps({
 
 <template>
   <div id="sidebar">
-    <p v-if="!clickedCoords || clickedCoords?.length == 0" class="fadein-top-down">Click a coordinate on the boord.</p>
-    <p v-else v-for="(coord, index) in clickedCoords" :key="index - clickedCoords.length" class="fadein-top-down">{{
-      coord
-      }}</p>
+    <h1 id="sidebar-title">Selection History</h1>
+    <hr>
+    <div id="sidebar-history">
+      <p v-if="!clickedCoords || clickedCoords?.length == 0" class="fadein-top-down">Click a coordinate on the boord.
+      </p>
+      <p v-else v-for="(coord, index) in clickedCoords" :key="index - clickedCoords.length"
+        class="fadein-top-down fit-content">{{
+          coord
+        }}</p>
+    </div>
   </div>
 </template>
 
 <style scoped>
+hr {
+  margin: 1rem 0 1rem 0;
+}
+
 #sidebar {
-  display: flex;
-  overflow-x: hidden;
-  overflow-y: auto;
-  flex-direction: column;
   width: 100%;
+  height: 100%;
+  overflow-x: hidden;
   flex-shrink: 0;
   padding: 1rem;
   border: solid 1px #aaa;
-  gap: 0.5rem;
-  background: #ddd;
-  height: 100%;
   border-radius: 0.375rem;
+  background: #ddd;
+}
+
+#sidebar-history {
+  display: flex;
+  overflow-y: auto;
+  flex-direction: column;
+  width: 100%;
+  gap: 0.5rem;
 }
 
 @media (min-width: 1024px) {
@@ -37,15 +51,19 @@ const props = defineProps({
 
 @keyframes slidein {
   0% {
-    transform: translateY(-2rem);
+    transform: translateX(calc(-100%));
   }
 
   100% {
-    transform: translateY(0);
+    transform: translateX(0);
   }
 }
 
 .fadein-top-down {
   animation: 0.3s slidein 1;
+}
+
+.fit-content {
+  width: fit-content;
 }
 </style>
